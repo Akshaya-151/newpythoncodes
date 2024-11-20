5d332e366402f712d0b066625b4543cb5f65ec29
Akshaya-151 <akshayavinod151@gmail.com>
2024-11-20 11:05:06 +0530

    this for module add

---
+++ addnum.py
@@ -0,0 +1,9 @@
+def add_number(a,b):
+	return a+b
+if __name__=="__main__":
+	num1=float(input("enter the first number:"))
+	num2=float(input("enter the second number:"))
+	result=add_number(num1,num2)
+	print(f"the sum of {num1}and {num2}is {result}")
+
+	
